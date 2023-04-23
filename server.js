b1b84c1b79cc9652cda7b8cf6f5451d78621e4c1
Alexander Zheng <69740012+alexz957unc@users.noreply.github.com>
2023-04-22 22:53:41 -0400

    Switch to variable names that make more sense

--- server.js
+++ server.js
@@ -11,45 +11,45 @@ const port = args.port || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
-app.get('/app', (req, res) => {
-    res.status(200).send('200 OK');
+app.get('/app', (request, respond) => {
+    respond.status(200).send('200 OK');
 })
 
-app.get('/app/rps', (req, res) => {
-    res.status(200).send(rps());
+app.get('/app/rps', (request, respond) => {
+    respond.status(200).send(rps());
 })
 
-app.get('/app/rpsls', (req, res) => {
-    res.status(200).send(rpsls());
+app.get('/app/rpsls', (request, respond) => {
+    respond.status(200).send(rpsls());
 })
 
-app.get('/app/rps/play', (req, res) => {
-    res.status(200).send(rps(req.query.shot));
+app.get('/app/rps/play', (request, respond) => {
+    respond.status(200).send(rps(request.query.shot));
 })
 
-app.get('/app/rpsls/play', (req, res) => {
-    res.status(200).send(rpsls(req.query.shot));
+app.get('/app/rpsls/play', (request, respond) => {
+    respond.status(200).send(rpsls(request.query.shot));
 })
 
-app.post('/app/rps/play', (req, res) => {
-    res.status(200).send(rps(req.body.shot));
+app.post('/app/rps/play', (request, respond) => {
+    respond.status(200).send(rps(request.body.shot));
 })
 
-app.post('/app/rpsls/play', (req, res) => {
-    res.status(200).send(rpsls(req.body.shot));
+app.post('/app/rpsls/play', (request, respond) => {
+    respond.status(200).send(rpsls(request.body.shot));
 })
 
 
-app.get('/app/rps/play/:shot', (req, res) => {
-    res.status(200).send(rps(req.params.shot));
+app.get('/app/rps/play/:shot', (request, respond) => {
+    respond.status(200).send(rps(request.params.shot));
 })
 
-app.get('/app/rpsls/play/:shot', (req, res) => {
-    res.status(200).send(rpsls(req.params.shot));
+app.get('/app/rpsls/play/:shot', (request, respond) => {
+    respond.status(200).send(rpsls(request.params.shot));
 })
 
-app.get('*', (req, res) => {
-    res.status(404).send('404 NOT FOUND');
+app.get('*', (request, respond) => {
+    respond.status(404).send('404 NOT FOUND');
   });
 
 app.listen(port, () => {
