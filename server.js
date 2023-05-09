c42a70e702b40a23a3c15776f78ed258830f6bf6
Alexander Zheng <69740012+alexz957unc@users.noreply.github.com>
2023-05-09 16:16:04 -0400

    New try

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { rps, rpsls } from "./lib/rpsls.js"
 import express from "express"
 import minimist from "minimist"
 
+
 const app = express();
 const args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
